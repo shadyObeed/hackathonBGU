624474ce24c86c10db86953b1800d3aa8999b5b8
shady <obeed@post.bgu.ac.il>
2020-12-31 20:04:13 +0200

    adding comments and make the code easy to read.

--- client.py
+++ client.py
@@ -15,7 +15,7 @@ end = "\033[0;1m"
 # thread that sends packets to the server
 def startingThread(sock):
     try:
-        endtime = time.time()+10
+        endtime = time.time() + 10
         while time.time() < endtime:
             try:
                 sock.settimeout(max(0, endtime - time.time()))
@@ -31,16 +31,19 @@ def startingThread(sock):
 # thread that listen to the server in the game so he can print the result of the game
 def ScoreOutput(sock):
     try:
-        endtime = time.time()+10
+        endtime = time.time() + 10
         while time.time() < endtime:
             output = sock.recv(1024)
             if output:
                 print(output.decode('utf-8'))
-    except Exception as e : print(e)
+    except :
+        pass
+
 
 def Main():
     TEAM_NAME = f"{Yellow}Bugs Bunny\n{end}"
     print(f"{Green}Client started,listening for offer requests...\n{end}")
+    # init udp connection
     client = UDPConn()
     try:
         client.bind(("", 13117))
@@ -51,14 +54,15 @@ def Main():
         data1, addr = client.recvfrom(1024)
         host, UDP_Port = addr
         try:
-            data1, data2 , TCP_Port = struct.unpack('!IBH', data1)
+            data1, data2, TCP_Port = struct.unpack('!IBH', data1)
             if data1 == 0xfeedbeef and data2 == 0x2:  # checking recieved message from broadcast
                 print(f"{Green}received offer from{end}", host, f"{Green},attempting to connect...\n{end}")
                 sock = TCPConn(TCP_Port, host)
                 try:
+                    # sending the team name
                     sock.sendall(TEAM_NAME.encode('utf-8'))
+                    # calling fun start the game
                     SendDataByThread(sock)
-                    # ScoreOutput(sock)
                 except:
                     print(f"{Red}server closed{end}")
                 finally:
@@ -100,8 +104,7 @@ def SendDataByThread(sock):
 
             DataIsFound = True
 
-
-
+# init udp connection that listen to UDP packets to get the broadcast
 def UDPConn():
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
